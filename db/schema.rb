7b9d5d59de84fce5ca595dc6628a614ceeaaf245
cristian <cristian982940@gmail.com>
2019-01-24 09:12:48 -0500

    commit en linux seguridad

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_01_23_212035) do
+ActiveRecord::Schema.define(version: 2019_01_23_224051) do
 
   create_table "books", force: :cascade do |t|
     t.string "title"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 2019_01_23_212035) do
     t.decimal "price"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "image_url"
   end
 
 end

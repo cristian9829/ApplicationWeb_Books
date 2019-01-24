cdf8bac465ea787a0771c17c0ff895a4030f6ade
cristian <cristian982940@gmail.com>
2019-01-24 15:30:36 -0500

    Add validates to model book

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,2 +1,5 @@
 class Book < ApplicationRecord
+	validates :title, :author, presence: true #Para validar si la informacion recibida no es vacia o nula
+	validates :price, numericality: true
+	validates :title, length: { minimun: 500 }
 end

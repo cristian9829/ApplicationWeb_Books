72620a2188fbd5403354cb4c4d22c33eaa574e98
cristian <cristian982940@gmail.com>
2019-01-24 18:35:55 -0500

    create asociaciones of tables comment, categoria and more, and submit mails

--- Gemfile
+++ Gemfile
@@ -5,7 +5,8 @@ ruby '2.5.3'
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '~> 5.2.2'
-
+# Prueba emails Letter Opener
+gem "letter_opener", group: :development
 # Use Puma as the app server
 gem 'puma', '~> 3.11'
 #Devise
--- app/models/book.rb
+++ app/models/book.rb
@@ -1,5 +1,7 @@
 class Book < ApplicationRecord
 	validates :title, :author, presence: true #Para validar si la informacion recibida no es vacia o nula
 	validates :price, numericality: true
-	validates :title, length: { minimun: 500 }
+	validates :title, length: { minimum: 2 }
+	belongs_to :category
+	has_many :comments
 end
